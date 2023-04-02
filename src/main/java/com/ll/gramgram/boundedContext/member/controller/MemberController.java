c7723c5a67ccfb09a141cbc7098732aa054a0b0d
YunJiW <dtroy371@gmail.com>
2023-04-02 17:30:03 +0900

    redirectiongWithMsg 적용

--- src/main/java/com/ll/gramgram/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/gramgram/boundedContext/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.ll.gramgram.boundedContext.member.controller;
 
+import com.ll.gramgram.base.rq.Rq;
 import com.ll.gramgram.base.rsData.RsData;
 import com.ll.gramgram.boundedContext.member.entity.Member;
 import com.ll.gramgram.boundedContext.member.service.MemberService;
@@ -25,6 +26,8 @@ import java.security.Principal;
 public class MemberController {
 
     private final MemberService memberService;
+
+    private final Rq rq;
 
     @GetMapping("/login")
     public String showLogin(){
@@ -55,11 +58,11 @@ public class MemberController {
         RsData<Member> joinRs = memberService.join(joinForm.getUsername(),joinForm.getPassword());
 
         if(joinRs.isFail()){
-            return "common/js";
+            //뒤로하기 하고 메세지 보여줘.
+            return rq.historyBack(joinRs);
         }
-        String msg = joinRs.getMsg() + "/n로그인 후 이용해주세요.";
-
-        return "redirect:/member/login?msg=" + Ut.url.encode(msg);
+        // 아래 링크로 리다이렉트(302, 이동) 하고 그 페이지에서 메세지 보여줘
+        return rq.redirectWithMsg("/member/login", joinRs);
     }
 
     @PreAuthorize("isAuthenticated()")
